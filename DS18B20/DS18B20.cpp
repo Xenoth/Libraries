804f36b9aac0da4f5b1b1fa91df78b7039424f95
Xenoth <polbailleux@gmail.com>
2016-09-01 00:37:07 +0200

    Add files via upload

---
+++ DS18B20/DS18B20.cpp
@@ -0,0 +1,56 @@
+#include "DS18B20.h"
+
+DS18B20::DS18B20(uint8_t pin)
+{
+    ds = new OneWire(pin);
+    isDetected = false;
+}
+
+DS18B20::~DS18B20()
+{
+    delete ds;
+}
+
+uint8_t DS18B20::search()
+{
+    if(!ds->search(address))
+    {
+        isDetected=false;
+        ds->reset_search();
+        return 1;
+    }
+    if(OneWire::crc8(address,7) != address[7])
+    {
+        isDetected=false;
+        return 2;
+    }
+    if(address[0]!=DS18B20_address)
+    {
+        isDetected=false;
+        return 3;
+    }
+
+    isDetected=true;
+    return 0;
+}
+
+boolean DS18B20::getTemp(double* temp)
+{
+    if(!this->detected())
+        return false;
+
+    ds->reset();
+    ds->select(address);
+    ds->write(0x44, 1);
+    delay(800);
+
+    ds->reset();
+    ds->select(address);
+    ds->write(0XBE);
+
+    for(byte i=0; i<9; i++)
+        data[i]=ds->read();
+
+    *temp = ((data[1] << 8) | data[0]) * 0.0625;
+    return true;
+}
---
+++ DS18B20/DS18B20.h
@@ -0,0 +1,38 @@
+#ifndef DS18B20_H
+#define DS18B20_H
+
+#define DS18B20_address 0x28
+
+#include <OneWire.h>
+
+class DS18B20 {
+
+    public:
+
+        DS18B20(uint8_t pin);
+        ~DS18B20();
+
+        /*
+        0: No error
+        1
+        1: Not a OneWire Module
+        2: Answer Corrupted
+        3: Not a DS18B20 module
+        */
+        uint8_t search();
+
+        inline boolean const detected(){ return isDetected; };
+        
+        boolean getTemp(double* temp);
+
+    private:
+
+        boolean isDetected;
+        OneWire* ds;
+        byte data[9], address[8];
+
+
+};
+
+
+#endif
