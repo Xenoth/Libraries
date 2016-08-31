2829cb8c4cfb28730931f93b48b8dc5c2b1655de
Xenoth <polbailleux@gmail.com>
2016-09-01 00:38:16 +0200

    Add files via upload

---
+++ HYSRF05/HYSRF05.cpp
@@ -0,0 +1,40 @@
+#include "HYSRF05.h"
+
+HYSRF05::HYSRF05(uint8_t trigPin, uint8_t echoPin)
+{
+    this->trigPin=trigPin;
+    this->echoPin=echoPin;
+}
+
+void HYSRF05::setup()
+{
+    pinMode(trigPin, OUTPUT);
+    digitalWrite(trigPin, LOW);
+    delay(50);
+    pinMode(echoPin, INPUT);
+}
+
+int HYSRF05::scan()
+{
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    int ms=pulseIn(echoPin, HIGH);
+    
+    delay(50);
+    
+    return miliseconds2Centimeters(ms);
+}
+
+boolean HYSRF05::inRange(int centimeters)
+{
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+
+    float msRange=centimeters2Miliseconds(centimeters);
+
+    float ms=pulseIn(echoPin, HIGH, msRange);
+    return (ms<=msRange);
+}
---
+++ HYSRF05/HYSRF05.h
@@ -0,0 +1,28 @@
+#ifndef HYSRF05_H
+#define HYSRF05_H
+
+#include <Arduino.h>
+
+class HYSRF05
+{
+    public:
+
+        HYSRF05(uint8_t trigPin, uint8_t echoPin);
+
+        void setup();
+
+        //return distance, 450 if no obstacle detected
+        int scan();
+
+        //Return true if obstacle detected in this range
+        boolean inRange(int centimeters);
+
+    private:
+
+        uint8_t trigPin, echoPin;
+
+        inline int const miliseconds2Centimeters(int ms){ return (ms/58); };
+        inline int const centimeters2Miliseconds(int cm){ return (cm*58); };
+};
+
+#endif
